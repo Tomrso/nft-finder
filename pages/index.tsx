5acfb17bbc399de54209ab46d0e75778d943cbcb
Tomrso <thomas.wilson98@hotmail.com>
2023-01-19 10:06:11 +1100

    modified:   README.md 	modified:   pages/index.tsx 	modified:   public/favicon.ico 	deleted:    public/next.svg 	deleted:    public/thirteen.svg 	deleted:    public/vercel.svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Connection, clusterApiUrl, Keypair, PublicKey} from "@solana/web3.js";
 import { Metaplex, keypairIdentity, bundlrStorage} from "@metaplex-foundation/js";
 import React from "react"
+import Head from "next/head"
 
 
 function createConnection(
@@ -32,28 +33,35 @@ export default function Home() {
   }
 
   return ( 
-    <div className="wrapper">
+    <div>
+      <Head>
+        <title>NFT Checker</title>
+        <link rel="shortcut-icon" type="image/ico" href="./favicon.ico"></link>
+      </Head>
+      <div className="wrapper">
 
-      <div className="header">
-        <h1>Solana NFT Checker</h1>
-      </div>
+        <div className="header">
+          <h1>Solana NFT Checker</h1>
+        </div>
 
-      <div className="form">
-        <h2>Enter Your NFT Token Address:</h2>
-        <input onChange={(event) => setTokenId(event.target.value)} type="text" />
-        <button onClick={fetchNft}>Check NFT</button>
-      </div>
+        <div className="form">
+          <h2>Enter Your NFT Token Address:</h2>
+          <input onChange={(event) => setTokenId(event.target.value)} type="text" />
+          <button onClick={fetchNft}>Check NFT</button>
+        </div>
 
-      {nft && 
-      <div className="nftCard">
-      <img src={nft.json.image} height="200px" width="auto" alt="alt text"/>
-      <h2>{nft.json.name}</h2>
-      <p>{nft.json.description}</p>
-      <a href={nft.json.external_url}>{nft.json.external_url}</a>
-      <h2>Traits</h2>
-      <div className="grid-container">
-        {nft.json.attributes.map(item => <article key="item.trait_type" className="grid-item"><strong>{item.trait_type}:</strong>{item.value}</article>)}
+        {nft && 
+        <div className="nftCard">
+            <img src={nft.json.image} height="200px" width="auto" alt="alt text"/>
+            <h2>{nft.json.name}</h2>
+            <p>{nft.json.description}</p>
+            <a href={nft.json.external_url}>{nft.json.external_url}</a>
+            <h2>Traits</h2>
+            <div className="grid-container">
+              {nft.json.attributes.map(item => <article key="item.trait_type" className="grid-item"><strong>{item.trait_type}:</strong>{item.value}</article>)}
+            </div>
+        </div>} 
+        <p>Built By <a href="https://twitter.com/tomrso">Tomrso</a></p>
       </div>
-      </div>} 
     </div>
 )}
